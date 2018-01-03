67e4fd4fb6b6ecb00d7e0e6033bacd00cd3f2a7b
voakgh <victorthinkahead@gmail.com>
2018-01-03 17:44:12 +0000

    all exercises done and ready for a try

--- src/pages/exercise/exercise.ts
+++ src/pages/exercise/exercise.ts
@@ -69,8 +69,83 @@ export class ExercisePage {
       });
       alert.present();
   }
+
+  radio5() {
+    let alert = this.alertCtrl.create({
+      title: 'Greet',
+      subTitle: 'Your answer is correct!',
+      buttons: ['OK']
+    });
+    alert.present();
+  }
+
+  radio6(){
+    
+      let alert = this.alertCtrl.create({
+        title: 'Try Again',
+        subTitle: 'You can do better than this',
+        buttons: ['OK']
+      });
+      alert.present();
+  }
+
+  radio7() {
+    let alert = this.alertCtrl.create({
+      title: 'Try Again',
+      subTitle: 'Kumasi is a city in Ghana, not the capital of Ghana.!',
+      buttons: ['OK']
+    });
+    alert.present();
+  }
   
-  
+  radio8(){
+    
+      let alert = this.alertCtrl.create({
+        title: 'Correct',
+        subTitle: 'Accra, is the capital of Ghana',
+        buttons: ['OK']
+      });
+      alert.present();
+  }
+
+  radio9() {
+
+    let alert = this.alertCtrl.create({
+      title: 'Correct',
+      subTitle: 'A noun is a name',
+      buttons: ['OK']
+    });
+    alert.present();
+  }
+
+  radio10() {
+    let alert = this.alertCtrl.create({
+      title: 'Try Again',
+      subTitle: 'A noun is a name of a person, place animal, thing emotions etc',
+      buttons: ['OK']
+    });
+    alert.present();
+  }
+
+  radio11() {
+
+    let alert = this.alertCtrl.create({
+      title: 'Correct',
+      subTitle: 'keeep the good up',
+      buttons: ['OK']
+    });
+    alert.present();
+  }
+
+  radio12() {
+    let alert = this.alertCtrl.create({
+      title: 'Try Again',
+      subTitle: '',
+      buttons: ['OK']
+    });
+    alert.present();
+  }
+
 
 
 }
