506484d6a06c7b5e43c3608a31db238ae4f33121
voakgh <victorthinkahead@gmail.com>
2018-01-03 15:36:37 +0000

    have added the exercise form to the exercise page

--- src/pages/exercise/exercise.ts
+++ src/pages/exercise/exercise.ts
@@ -7,6 +7,11 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
  * See https://ionicframework.com/docs/components/#navigation for more info on
  * Ionic pages and navigation.
  */
+import {
+  FormGroup,
+  FormControl
+
+} from '@angular/forms';
 
 @IonicPage()
 @Component({
@@ -15,11 +20,28 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class ExercisePage {
 
+  langs;
+  langForm;
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.langForm = new FormGroup({
+      "langs": new FormControl({ value: 'rust', disabled: false })
+    });
+  }
+
+  doSubmit(event) {
+    console.log('Submitting form', this.langForm.value);
+    event.preventDefault();
+  }
+
+  radioChecked(){
+    
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ExercisePage');
   }
+
+
 
 }
